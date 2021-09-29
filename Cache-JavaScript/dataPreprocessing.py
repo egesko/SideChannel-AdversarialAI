eca5e214a357e4c5849796f2df50cd486e8ea43b
Ege <ege@iastate.edu>
2021-09-29 16:11:36 -0500

    Finished create_Dataset function, added comments

--- Cache-JavaScript/dataPreprocessing.py
+++ Cache-JavaScript/dataPreprocessing.py
@@ -6,7 +6,7 @@ import os
 
 
 
-def append_list_as_row(target_file_name, source_file_name):
+def append_list_as_row(target_file_name, source_file_name): #Helper function. Purpose: appending one csv file to another
 	with open(target_file_name,'a',newline = '\n') as fd:
 		with open(source_file_name ,newline='') as sourceFile:
 
@@ -23,13 +23,13 @@ def append_list_as_row(target_file_name, source_file_name):
 					fd.write(','+str(data))
 				i += 1
 
-def change_extremes_in_csv(target_file_name):
+def change_extremes_in_csv(target_file_name,num): #If you want to change datapoints above a predefined number, use this function
 	with open(target_file_name,newline = '') as sourceFile:
 		data_list = list(np.loadtxt(sourceFile, delimiter = ','))
 
 		for row_index, row in enumerate(data_list):
 			for col_index, item in enumerate(row):
-				if(item > 1):
+				if(item > num):
 					data_list[row_index][col_index] = 1
 
 		with open("train_normalized.csv","w",newline='') as my_csv:
@@ -37,7 +37,7 @@ def change_extremes_in_csv(target_file_name):
 		    csvWriter.writerows(data_list)
 
 
-def normalize(target_file_name,max,min):
+def normalize(target_file_name,source_file_name,max,min): # Normalizing all values in a csv file 
 	with open(target_file_name,newline = '') as sourceFile:
 		data_list = list(np.loadtxt(sourceFile, delimiter = ','))
 
@@ -45,39 +45,39 @@ def normalize(target_file_name,max,min):
 			for col_index, item in enumerate(row):
 					data_list[row_index][col_index] = (item-min)/(max-min) #normalizing function, change if necessary
 
-		with open("train_val_normalized.csv","w",newline='') as my_csv:
+		with open(source_file_name,"w",newline='') as my_csv:
 		    csvWriter = csv.writer(my_csv,delimiter=',')
 		    csvWriter.writerows(data_list)
 
 
+def append_Label(target,number): # Helper function for appending labels (element of positive integers) to a target csv file
+	with open(target,'a',newline = '\n') as fd:
+		fd.write('\n')
+		fd.write(str(number))
 
-def create_DataSet_From_Folder(folder,valAmt):
+
+def create_DataSet_From_Folder(folder,finalTrainX,finalTrainY): #Creates DataSet from a directory. Also shuffles the samples.
 
 	websiteAmount = len(os.listdir(folder))
-	print('websiteAmount: '+ str(websiteAmount))
-
 
 	listForAmount = os.listdir(folder)
 	dataPerWebsite = len(os.listdir(folder + '/' + listForAmount[0]))
 
 	for i in range(dataPerWebsite):
+		num = 0
 		for websiteName in os.listdir(folder):
 			
-			mylist = os.listdir(folder + '/' + websiteName)
+			dataListForWebsite = os.listdir(folder + '/' + websiteName)
 
-			print(len(mylist))
+			append_list_as_row(finalTrainX,folder+'/'+websiteName+'/'+dataListForWebsite[i])
+			append_Label(finalTrainY,num)
+			num += 1
 			
-
-			
-		# for filename in os.listdir(folder + '/' + folderName):
-		# 	append_list_as_row(target,folder+ '/' + folderName + '/' +filename)
+		
 
 
-
-
-#create_DataSet_From_Folder('try3')
+create_DataSet_From_Folder('try3','TrainX.csv','TrainY.csv')
 #append_list_as_row('ssdec21-13_1629917674731.csv','ssdec21-13_1629917644600.csv')
-
 #normalize('train_val.csv',29.7,1.3)
 
 
